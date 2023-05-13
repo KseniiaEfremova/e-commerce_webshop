dc24c30e2e69c1521032ba0893204201789bb676
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-05-13 19:12:49 +0100

    Added footer, header and initial styles

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo' >
+                <div>FLOOM</div>
+                <ul>
+                    <li>HOME</li>    
+                    <li>OUR PRODUCTS</li>  
+                    <li>CONTACT US</li>
+                </ul>
+                <div>Basket</div>
+            </span>
+        </div>
+        <div className='presentation'></div>
+      
+    </header>
+  )
+}

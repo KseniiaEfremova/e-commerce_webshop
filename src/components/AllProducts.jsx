45eee6b28ae070efcdaf26ad1d363229c013a542
KSENIIA EFREMOVA <ef.xenya@yandex.ru>
2023-05-16 14:43:30 +0100

    Added styles for OUR products page

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import data from '../products.json';
-import '../styles/allProducts.css'
+import '../styles/allProducts.css';
 
+// const product = data.map(item => (
+//   <ProductItem key={item.pk.id}
+//             name={item.fields.name} 
+//             url={item.fields.image_url} 
+//             description={item.fields.description} />
+//  ))
 
 const AllProducts = () => {
   return (
@@ -10,43 +16,55 @@ const AllProducts = () => {
       <div className='grid-wrapper'>
         <div className='product-card'>
           <img src="http://s7d9.scene7.com/is/image/JCPenney/DP0709201205510679M.tif?hei=380&amp;wid=380&op_usm=.4,.8,0,0&resmode=sharp2&op_usm=1.5,.8,0,0&resmode=sharp" alt="" />
-          <p>Arizona Original Bootcut Jeans</p>
+          <h5>Arizona Original Bootcut Jeans</h5>
           <div className='card-bottom'>
             <span>Price:<b>23.45 $</b></span>
             <button>+</button>
           </div>
-          
         </div>
         <div className='product-card'>
           <img src="http://s7d9.scene7.com/is/image/JCPenney/DP0709201205510679M.tif?hei=380&amp;wid=380&op_usm=.4,.8,0,0&resmode=sharp2&op_usm=1.5,.8,0,0&resmode=sharp" alt="" />
-          <p>Arizona Original Bootcut Jeans</p>
-          <div>
-            <span>Price:</span>
-            <b>23.45 $</b>
+          <h5>Arizona Original Bootcut Jeans</h5>
+          <div className='card-bottom'>
+            <span>Price:<b>23.45 $</b></span>
+            <button>+</button>
           </div>
-
-          <button>+</button>
         </div>
         <div className='product-card'>
           <img src="http://s7d9.scene7.com/is/image/JCPenney/DP0709201205510679M.tif?hei=380&amp;wid=380&op_usm=.4,.8,0,0&resmode=sharp2&op_usm=1.5,.8,0,0&resmode=sharp" alt="" />
-          <p>Arizona Original Bootcut Jeans</p>
-          <div>
-            <span>Price:</span>
-            <b>23.45 $</b>
+          <h5>Arizona Original Bootcut Jeans</h5>
+          <div className='card-bottom'>
+            <span>Price:<b>23.45 $</b></span>
+            <button>+</button>
           </div>
-
-          <button>+</button>
         </div>
         <div className='product-card'>
           <img src="http://s7d9.scene7.com/is/image/JCPenney/DP0709201205510679M.tif?hei=380&amp;wid=380&op_usm=.4,.8,0,0&resmode=sharp2&op_usm=1.5,.8,0,0&resmode=sharp" alt="" />
-          <p>Arizona Original Bootcut Jeans</p>
-          <div>
-            <span>Price:</span>
-            <b>23.45 $</b>
+          <h5>Arizona Original Bootcut Jeans</h5>
+          <div className='card-bottom'>
+            <span>Price:<b>23.45 $</b></span>
+            <button>+</button>
           </div>
-
-          <button>+</button>
         </div>
+        <div className='product-card'>
+          <img src="http://s7d9.scene7.com/is/image/JCPenney/DP0709201205510679M.tif?hei=380&amp;wid=380&op_usm=.4,.8,0,0&resmode=sharp2&op_usm=1.5,.8,0,0&resmode=sharp" alt="" />
+          <h5>Arizona Original Bootcut Jeans</h5>
+          <div className='card-bottom'>
+            <span>Price:<b>23.45 $</b></span>
+            <button>+</button>
+          </div>
+        </div>
+        <div className='product-card'>
+          <img src="http://s7d9.scene7.com/is/image/JCPenney/DP0709201205510679M.tif?hei=380&amp;wid=380&op_usm=.4,.8,0,0&resmode=sharp2&op_usm=1.5,.8,0,0&resmode=sharp" alt="" />
+          <h5>Arizona Original Bootcut Jeans</h5>
+          <div className='card-bottom'>
+            <span>Price:<b>23.45 $</b></span>
+            <button>+</button>
+          </div>
+        </div>
+
+        
+
       </div>
 
 
